f8433080703ba9496b215bd3287df35e65ea0aee
Roy Kirk <ricotalzier@gmail.com>
2017-03-22 11:39:26 +1100

    fixed menus

--- SpaceDragon/Assets/MenuActions.cs
+++ SpaceDragon/Assets/MenuActions.cs
@@ -1,21 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MenuActions : MonoBehaviour {
+    public GameObject defaultButton;
+
     public void Restart()
     {
         Time.timeScale = 1.0f;
         SceneManager.LoadScene("PlayerControls");
+        Cursor.visible = false;
     }
     public void Resume()
     {
         gameObject.SetActive(false);
         Time.timeScale = 1.0f;
+        Cursor.visible = false;
     }
     public void Exit()
     {
         Application.Quit();
+    }
+    public void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(defaultButton);
+        }
     }
 }
