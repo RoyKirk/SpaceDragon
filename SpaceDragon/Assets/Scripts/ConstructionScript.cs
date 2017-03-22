c9d4fc2650457cb55db7c8a7210f042aed047f98
Zac <zac.molyneux@hotmail.com>
2017-03-23 10:36:20 +1100

    added turret and ability to Construct.

--- SpaceDragon/Assets/Scripts/ConstructionScript.cs
+++ SpaceDragon/Assets/Scripts/ConstructionScript.cs
@@ -1,16 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
+using UnityEngine.UI;
 
-public class ConstructionScript : MonoBehaviour {
+public class ConstructionScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    PlayerMovement PlayerM;
+    private Player player;
+    int playerId = 0;
+
+
+    int dirtCount;
+    int metalCount;
+    int ammoCount;
+
+
+    public GameObject turret;
+    public int turretCost = 10;
+
+    public Text ErrorMessage;
+    bool setMessage = false;
+    float messageDuration = 2;
+    float messageTimer;
+    
+
+    // Use this for initialization
+    void Start ()
+    {
+        PlayerM = GetComponent<PlayerMovement>();
+        playerId = PlayerM.playerId;
+        player = ReInput.players.GetPlayer(playerId);
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        dirtCount = PlayerM.blockCount;
+        metalCount = PlayerM.metalCount;
+
+        if(setMessage)
+        {
+            messageTimer -= Time.deltaTime;
+        }
+        else
+        {
+            messageTimer = messageDuration;
+            setMessage = false;
+        }
+
+        if (messageTimer <= 0)
+        {
+            SetErrorMessage("");
+        }
+        
+
+
+        if(player.GetButtonDown("Alt Fire"))
+        {
+            //placing a turret
+            if(metalCount >= turretCost)
+            {
+                Instantiate(turret, transform.position, transform.rotation);
+                PlayerM.metalCount -= turretCost;
+            }
+            else
+            {
+                SetErrorMessage("Not Enough Metal");
+            }
+            
+        }
+    }
+
+    void SetErrorMessage(string message)
+    {
+        ErrorMessage.text = message;
+        if(message != "")
+        {
+            setMessage = true;
+        }
+    }
 }
