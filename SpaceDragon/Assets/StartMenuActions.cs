a320b25ea19e4a53d9a95ab2146632a53986f255
Roy Kirk <ricotalzier@gmail.com>
2017-04-04 13:23:14 +1000

    start menu scene

---
+++ SpaceDragon/Assets/StartMenuActions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class StartMenuActions : MonoBehaviour {
+    public GameObject defaultButton;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("PlayerControls");
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(defaultButton);
+        }
+    }
+}
